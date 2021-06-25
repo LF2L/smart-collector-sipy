aca25430cc5fb7e507552879446bdc09f5fd4bab
GABRIEL Alex <alex.gabriel@univ-lorraine.fr>
2021-06-25 15:54:53 +0200

    clarified the main

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ import ujson
 from network import Sigfox
 import socket
 
+uplink_intervalle = 12 # minutes
+
 def measure():
     value = hx711.get_value()/394.786
     print("{} g".format(int(value)))
     return value
 
-def sendData(data):
+def sendData(weightData, batteryData):
+    print("Start the data sending procedure")
     # init Sigfox for RCZ1 (Europe)
     sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
 
@@ -25,74 +28,109 @@ def sendData(data):
     # configure it as uplink only
     s.setsockopt(socket.SOL_SIGFOX, socket.SO_RX, False)
 
-    # send some bytes
-    print("raw data: {}".format(data))
-    data_in_int= int(data)
+    # convert weight data
+    data_in_int= int(weightData)
     data_in_bytes = data_in_int.to_bytes(2,'big')
 
-    #s.send(bytes([data]))
-    s.send(data_in_bytes)
+    # convert battery data
+    batt_data_in_int = int(batteryData)
+    batt_data_in_bytes = batt_data_in_int.to_bytes(2,'big')
+
+    # send the data through sigfox
+    s.send(data_in_bytes +batt_data_in_bytes )
 
 def tare_sensor():
     # tare the sensor
     hx711.tare()
+
     # create a dictionnary with tare info 
     persistant_data = {'offset': hx711.OFFSET}
+
     # save it into persistant memory
     rtc.memory(ujson.dumps(persistant_data))
 
+def procedure():
+    # get the data from the persistant memory
+    raw_data = rtc.memory()
+    # if the memory is not empty
+    if raw_data  != b'':
+
+        pycom.rgbled(0x007f00) # green light
+        print("Something in the persistant memory")
+        
+        # convert the data from the memory 
+        persistant_data = ujson.loads(raw_data)
+        # set the offset of the sensor (tare)
+        hx711.set_offset(persistant_data['offset'])
+
+        # measure the weight
+        weight = measure()
+
+        # measure the battery state
+        battVal = apin() 
+        print("weight: {}".format(weight))
+
+        # call the function to send data
+        sendData(int(weight), battVal)
+
+    else:
+        pycom.rgbled(0x7f0000) # RED light
+        # if it is empty, display a red light for 5s 
+        print("Nothing in the persistant memory: No tare")
+
+
+
+##############  MAIN #############
 print("Wake up")
 
+# Initialize RTC
 rtc = machine.RTC()
-pycom.heartbeat(False)
-Pin('P6', mode=Pin.IN, pull=Pin.PULL_DOWN)
 
-# get the reason why MCU is waking up
-(wake_reason, gpio_list) = machine.wake_reason()
+# Initialize ADC
+adc = machine.ADC()
+
+# Affect the pin
+apin = adc.channel(pin='P16')
+Pin('P23', mode=Pin.IN, pull=Pin.PULL_DOWN)
 
 # init the sensor
 hx711 = HX711('P3','P4')
 
+# disable LED blinking
+pycom.heartbeat(False)
+
+# get the reason why MCU is waking up
+(wake_reason, gpio_list) = machine.wake_reason()
+
+# MCU woke up through the reset button on the Lopy4 board
 if wake_reason == machine.PWRON_WAKE:
-    print("Woke up by reset button")
     pycom.rgbled(0x007f7f)
+    print("Woke up by reset button")
+    procedure()
+
+# MCU woke up through the external tare button
 elif wake_reason == machine.PIN_WAKE:
-    print("Woke up by external pin (external interrupt)")
     pycom.rgbled(0x00007f)
+    print("Woke up by external pin (external interrupt)")
+    # print in log which button triggered the wakeup
     print(*gpio_list, sep=", ")
+
     # call the function to tare 
     tare_sensor()
+    procedure()
      
 elif wake_reason == machine.RTC_WAKE:
+    pycom.rgbled(0xFFF81B)
     print("Woke up by RTC (timer ran out)")
-    # Load data from persistant memory 
-    raw_data = rtc.memory()
-    # chech if the memory is not empty
-    if raw_data  != b'':
-        # display a green light 
-        pycom.rgbled(0x007f00)
-        # convert the data from the memory 
-        persistant_data = ujson.loads(raw_data)
-        # set the sensor correctly
-        hx711.set_offset(persistant_data['offset'])
-        # do a measure
-        weight = measure()
 
-        # send the information
-        pybytes.send_signal(3, int(weight))
-        sendData(int(weight))
-    else:
-        # if it is empty, display a red light for 5s 
-        pycom.rgbled(0x7f0000)
-        time.sleep(5)
+    procedure()
 
 # wait several second just for development
 time.sleep(5)
 
 print("Deep sleep")
 # setup the way to manually wake up. enable_pull= True disable ULP or capacitive touch wakeup
-machine.pin_sleep_wakeup(('P6',), mode=machine.WAKEUP_ANY_HIGH, enable_pull=True)
+machine.pin_sleep_wakeup(('P23',), mode=machine.WAKEUP_ANY_HIGH, enable_pull=True)
 
 # start the deepsleep for a defined duration
-#machine.deepsleep(1000*60) # 1 min
-#machine.deepsleep(1000*60*12) # 12 min
+machine.deepsleep(1000*60*uplink_intervalle) # 12 min
