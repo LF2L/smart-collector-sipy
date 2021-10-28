07a9b7ffa863545a2f92e403acbca15e5f17a3c2
GABRIEL Alex <alex.gabriel@univ-lorraine.fr>
2021-10-28 13:12:53 +0200

    fixed the measured error (wait 0.5 sec)

--- main.py
+++ main.py
@@ -4,19 +4,16 @@ import machine
 from machine import Pin
 from hx711 import HX711
 import ujson
+# required for Sigfox
 from network import Sigfox
 import socket
 
 uplink_intervalle = 20 # minutes
-calibration_factor = 423.6 #430.3
-production = False
-
-def measure(loadCell):
-    value = loadCell.get_units(10)
-    return value
+calibration_factor = 423.6 
+production = True
+communicationEnabled = True
 
 def sendData(weightData, batteryData):
-    if not production: print("Start the data sending procedure")
     # init Sigfox for RCZ1 (Europe)
     sigfox = Sigfox(mode=Sigfox.SIGFOX, rcz=Sigfox.RCZ1)
 
@@ -40,22 +37,20 @@ def sendData(weightData, batteryData):
     # send the data through sigfox
     s.send(data_in_bytes +batt_data_in_bytes )
 
-def tare_sensor(loadCell):
-    time.sleep(0.5)
+def tare_sensor(loadCellRef):    
     # tare the sensor
-    loadCell.tare()
-
-    if not production: print("Offset measured: "+ str(loadCell.OFFSET))
+    loadCellRef.tare()
 
     # create a dictionnary with tare info 
-    persistant_data = {'offset': loadCell.OFFSET}
+    persistant_data = {'offset': loadCellRef.OFFSET}
 
     # save it into persistant memory
     rtc.memory(ujson.dumps(persistant_data))
 
-def procedure(loadCell):
+def weight_measure(loadCellRef):
     # get the data from the persistant memory
     raw_data = rtc.memory()
+
     # if the memory is not empty
     if raw_data  != b'':
 
@@ -65,20 +60,19 @@ def procedure(loadCell):
         # convert the data from the memory 
         persistant_data = ujson.loads(raw_data)
 
-        if not production: print("Persistant offset = {}".format(persistant_data['offset']))
         # set the offset of the sensor (tare)
-        loadCell.set_offset(persistant_data['offset'])
-        if not production: print("scale: {}".format(loadCell.SCALE))
-        if not production: print("offset: {}".format(loadCell.OFFSET))
-        # measure the weight
-        weight = measure(loadCell)
+        loadCellRef.set_offset(persistant_data['offset'])
 
-        # measure the battery state
+        # weight_measure the weight
+        weight = str(loadCellRef.get_units(10))
+
+        # weight_measure the battery state
         battVal = apin.voltage()
         if not production: print("weight: {} g".format(weight))
         
         # call the function to send data
-        sendData(int(weight), battVal)
+        if communicationEnabled : sendData(int(weight), battVal)
+
         pycom.rgbled(0x000000) # black light
 
     else:
@@ -102,7 +96,6 @@ adc = machine.ADC()
 
 # Affect the pin
 apin = adc.channel(pin='P16')
-wakeupPin = Pin('P23', mode=Pin.IN, pull=Pin.PULL_DOWN)
 
 # init the sensor
 loadCell = HX711('P3','P4')
@@ -115,7 +108,7 @@ loadCell.set_scale(calibration_factor)
 if wake_reason == machine.PWRON_WAKE:
     pycom.rgbled(0x007f7f)
     if not production: print("Woke up by reset button")
-    procedure(loadCell)
+    weight_measure(loadCell)
 
 # MCU woke up through the external tare button
 elif wake_reason == machine.PIN_WAKE:
@@ -124,20 +117,26 @@ elif wake_reason == machine.PIN_WAKE:
     # print in log which button triggered the wakeup
     #if not production: print(*gpio_list, sep=", ")
 
+    # wait several microsec that power supply stabilize in order to do a correct tare
+    time.sleep(0.5)
+
     # call the function to tare 
     tare_sensor(loadCell)
-    procedure(loadCell)
+    weight_measure(loadCell)
      
 elif wake_reason == machine.RTC_WAKE:
     pycom.rgbled(0xFFF81B)
     if not production: print("Woke up by RTC (timer ran out)")
-    procedure(loadCell)
+
+    # wait several microsec that power supply stabilize in order to do correct measure
+    time.sleep(0.5)
+
+    weight_measure(loadCell)
 
 if not production: print("Deep sleep")
+wakeupPin = Pin('P23', mode=Pin.IN, pull=Pin.PULL_DOWN)
 # setup the way to manually wake up. enable_pull= True disable ULP or capacitive touch wakeup
 machine.pin_sleep_wakeup((wakeupPin,), mode=machine.WAKEUP_ANY_HIGH, enable_pull=True)
-
-# print("process time: ", time.ticks_diff(time.ticks_us(), start))
 
 # start the deepsleep for a defined duration
 machine.deepsleep(1000*60*uplink_intervalle)
